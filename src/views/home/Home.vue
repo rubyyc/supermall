0e3bd8a57dfea106809700c910e1a96001d14a0c
YuanChang <759337828@qq.com>
2021-04-17 09:52:37 +0800

    完成推荐信息

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,17 +6,20 @@
     </div>
   </nav-bar>
   <home-swiper :banners="banners"></home-swiper>
+  <recommend-view :recommends="recommends"></recommend-view>
 </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComponents/HomeSwiper'
+import RecommendView from './childComponents/RecommendView'
 import { getHomeMultiData } from 'network/home'
 
 export default {
   name: 'Home',
   components: {
+    RecommendView,
     NavBar,
     HomeSwiper
   },
