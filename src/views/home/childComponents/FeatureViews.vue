ec2d51aab80f3607be78bfe0b48098332eca6fd7
YuanChang <759337828@qq.com>
2021-04-17 11:29:16 +0800

    完成本周流行

---
+++ src/views/home/childComponents/FeatureViews.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+    <img src="" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureViews'
+}
+</script>
+
+<style scoped>
+.feature img{
+  width: 100%;
+}
+</style>
