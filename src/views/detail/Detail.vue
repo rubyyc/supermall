32ba342cd06feb7c63ee99dd2849bc781430245d
YuanChang <759337828@qq.com>
2021-04-19 21:51:32 +0800

    完成商品简单描述

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,19 +2,22 @@
   <div class="detail">
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComponents/DetailNavBar'
 import DetailSwiper from './childComponents/DetailSwiper'
-import { getDetail } from 'network/detail'
+import DetailBaseInfo from './childComponents/DetailBaseInfo'
+import { getDetail, Goods } from 'network/detail'
 export default {
   name: 'Detail',
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {}
     }
   },
   created() {
@@ -27,16 +30,19 @@ export default {
     if (this.iid !== this.$route.params.iid) {
       this.iid = this.$route.params.iid
       getDetail(this.iid).then(res => {
+        const data = res.result
         console.log(res)
         // 获取顶部轮播数据
-        this.topImages = res.result.itemInfo.topImages
+        this.topImages = data.itemInfo.topImages
         console.log(this.topImages)
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
       })
     }
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   }
 }
 </script>
