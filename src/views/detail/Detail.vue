6ef37742d1b11cb2d7b930d8c4ce5fc0433e3d12
YuanChang <759337828@qq.com>
2021-04-19 22:04:46 +0800

    完成店铺信息的展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,6 +3,7 @@
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
     <detail-base-info :goods="goods"></detail-base-info>
+    <detail-shop-info :shop="shop"></detail-shop-info>
   </div>
 </template>
 
@@ -10,14 +11,16 @@
 import DetailNavBar from './childComponents/DetailNavBar'
 import DetailSwiper from './childComponents/DetailSwiper'
 import DetailBaseInfo from './childComponents/DetailBaseInfo'
-import { getDetail, Goods } from 'network/detail'
+import DetailShopInfo from './childComponents/DetailShopInfo'
+import { getDetail, Goods, Shop } from 'network/detail'
 export default {
   name: 'Detail',
   data() {
     return {
       iid: null,
       topImages: [],
-      goods: {}
+      goods: {},
+      shop: {}
     }
   },
   created() {
@@ -31,18 +34,22 @@ export default {
       this.iid = this.$route.params.iid
       getDetail(this.iid).then(res => {
         const data = res.result
-        console.log(res)
+        // console.log(res)
         // 获取顶部轮播数据
         this.topImages = data.itemInfo.topImages
-        console.log(this.topImages)
+        // console.log(this.topImages)
+        // 获取商品简单描述
         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+        // 获取商品的商店的信息
+        this.shop = new Shop(data.shopInfo)
       })
     }
   },
   components: {
     DetailNavBar,
     DetailSwiper,
-    DetailBaseInfo
+    DetailBaseInfo,
+    DetailShopInfo
   }
 }
 </script>
