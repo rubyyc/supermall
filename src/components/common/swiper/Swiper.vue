b68a93c389f702d39a79ec75cc051bfba4c095be
YuanChang <759337828@qq.com>
2021-04-20 12:38:05 +0800

    尝试修复商品详情页的轮播图

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -50,7 +50,7 @@ export default {
 
       // 2.开启定时器
       this.startTimer()
-    }, 1000)
+    }, 800)
   },
   methods: {
     /**
--- src/views/detail/childComponents/DetailSwiper.vue
+++ src/views/detail/childComponents/DetailSwiper.vue
@@ -1,7 +1,7 @@
 <template>
-  <swiper  class="detail-swiper">
-    <swiper-item v-for="(item, index) in topImages" :key="index">
-      <img :src="item" alt="">
+  <swiper class="detail-swiper">
+    <swiper-item v-for="item in topImages" :key="item">
+      <img :src="item" alt="" :key="item">
     </swiper-item>
   </swiper>
 </template>
