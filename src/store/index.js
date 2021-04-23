8242b630b74fedc953863c95174d36124a31db01
YuanChang <759337828@qq.com>
2021-04-23 17:53:01 +0800

    vuex中getter的使用方法

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,13 @@ import Vuex from 'vuex'
 import mutations from './mutations'
 import * as actions from './actions'
 import state from './state'
-
+import getters from './getters'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state,
   mutations,
-  getters: {
-  },
+  getters,
   actions
 })
 
--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,15 +1,32 @@
 <template>
-<div>
-  <h2>购物车</h2>
-</div>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center">
+        购物车({{cartLength}})
+      </div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import { mapGetters } from 'vuex'
 export default {
-  name: 'Cart'
+  name: 'Cart',
+  computed: {
+    ...mapGetters([
+      'cartLength',
+      'cartList'
+    ])
+  },
+  components: { NavBar }
 }
 </script>
 
 <style scoped>
-
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+  font-weight: 700;
+}
 </style>
