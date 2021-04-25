7aef141f93b9a071a455a95e29aa60be7e58567e
YuanChang <759337828@qq.com>
2021-04-25 11:15:15 +0800

    vue-lazyload懒加载插件的使用

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import router from './router'
 import store from './store'
 import Toast from './components/common/toast'
 import FastClick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 Vue.use(Toast)
 
 FastClick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
